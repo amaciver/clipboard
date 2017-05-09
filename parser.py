b032d0ed197e8e2fcf714f010b99c580ed2cfafd
Andrew MacIver <maciver.andrew@gmail.com>
2017-05-08 19:18:46 -0700

    pandas df

--- parser.py
+++ parser.py
@@ -1,5 +1,12 @@
 import csv
-with open('projectnurse.csv', newline='\n') as csvfile:
-    nurseinfo = csv.reader(csvfile, delimiter=',')
-    for row in nurseinfo:
-        print(',\n '.join(row))
+import pandas as pd
+import numpy as np
+
+# with open('projectnurse.csv', newline='\n') as csvfile:
+    # nurseinfo = csv.reader(csvfile, delimiter=',')
+    # for row in nurseinfo:
+    #     print(',\n '.join(row))
+
+
+df = pd.read_csv('projectnurse.csv')
+print(df)
